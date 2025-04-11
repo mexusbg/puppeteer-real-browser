86873e1f7b748bb64e8b665a6e527fef8b30213f
mexus <mexusbg@gmail.com>
2025-04-11 17:34:24 +0300

    feat: add chromePort option to connect function for customizable Chrome port

--- typings.d.ts
+++ typings.d.ts
@@ -24,6 +24,7 @@ declare module "puppeteer-real-browser" {
 		disableXvfb?: boolean;
 		plugins?: import("puppeteer-extra").PuppeteerExtraPlugin[];
 		ignoreAllFlags?: boolean;
+		chromePort?: number;
 	}
 
 	interface ProxyOptions {
